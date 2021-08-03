70d25302ce7093cd7bdbd4802e4787cd95f9634d
Lucas <lwmoehlenbrock@ucdavis.edu>
2021-08-03 15:39:44 -0500

    changed to environment variables

--- src/main/java/com/codedifferently/groupone/SpyGlass/email/EmailService.java
+++ src/main/java/com/codedifferently/groupone/SpyGlass/email/EmailService.java
@@ -3,6 +3,7 @@ package com.codedifferently.groupone.SpyGlass.email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -19,6 +20,9 @@ public class EmailService implements EmailSender{
     private Logger log = LoggerFactory.getLogger(EmailService.class);
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JavaMailSender javaMailSender;
 
     @Override
@@ -26,10 +30,10 @@ public class EmailService implements EmailSender{
     public void send(String to, String email) {
         try{
             JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-            mailSender.setHost("smtp.gmail.com");
-            mailSender.setPort(587);
-            mailSender.setUsername("spyglassgroup1@gmail.com");
-            mailSender.setPassword("Test1956");
+            mailSender.setHost(env.getProperty("spring.mail.host"));
+            mailSender.setPort(Integer.parseInt(env.getProperty("spring.mail.port")));
+            mailSender.setUsername(env.getProperty("spring.mail.username"));
+            mailSender.setPassword(env.getProperty("spring.mail.password"));
             Properties props = mailSender.getJavaMailProperties();
             props.put("mail.transport.protocol", "smtp");
             props.put("mail.smtp.auth", "true");
