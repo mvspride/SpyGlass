47b7e2345bdad65640a904041bbe77ba0c63515a
studenteng01 <9313>
2021-08-04 13:48:51 -0400

    everything execpt the service is upto date

--- src/main/java/com/codedifferently/groupone/SpyGlass/SpyGlassApplication.java
+++ src/main/java/com/codedifferently/groupone/SpyGlass/SpyGlassApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpyGlassApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hellp");
 		SpringApplication.run(SpyGlassApplication.class, args);
 	}
 
